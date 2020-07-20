d40f9aa1439304bb67cd709cc07189a121d4f557
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-07-20 17:06:19 +0530

    Initial commit

---
+++ src/test/java/com/ab/cryptoportfolio/CryptoportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.cryptoportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
